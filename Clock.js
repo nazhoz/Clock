648a7b1cccc7cb704806d266c292a0c5f520528a
nazhoz <chemmalaniyas1145@gmail.com>
2023-10-25 13:06:17 +0530

    clock

--- Clock.js
+++ Clock.js
@@ -4,7 +4,14 @@ let sec = document.getElementById('sec')
 
 setInterval(() => {
     let currentTime = new Date();
-    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    let hours = currentTime.getHours()
+    let am_pm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12;
+    hours = hours ? hours :12;
+
+    hrs.innerHTML = (hours < 10 ? "0" : "") + hours
     min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds()
+    am_pm.innerHTML = am_pm
 }, 1000);
